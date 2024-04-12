e84e4ba52f689ea4aaa69cef62c6943373da5684
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 15:35:32 +0530

    #1219 LaneKeepAssist.js
    Test alert functionality under various driving conditions to ensure effectiveness and clarity.

---
+++ LaneKeepAssist.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Lane Keep Assist</h1>
+  <Script>document.write([TASK-1219] Test alert functionality under various driving conditions to ensure effectiveness and clarity    
+)</Script>
+</body>
+</html>
