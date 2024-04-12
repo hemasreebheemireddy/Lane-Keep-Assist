f876ff9d0a167511486f1dd894f2d0c382839ce1
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 15:33:43 +0530

    #1218 LaneKeepAssist.java
    Design visual and auditory alert cues for lane departure and Lane Keep Assist activation.

---
+++ LaneKeepAssist_1.java
@@ -0,0 +1,7 @@
+public class ObstacleDetection {
+    public static void main(String[] args) {
+        System.out.println(" [TASK-1218] Design visual and auditory alert cues for lane departure and Lane Keep Assist activation  
+");
+       
+    }
+}
